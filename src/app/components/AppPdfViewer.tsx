33b4559c0d09b363e5a5f70049e2425d8bd27db4
terawuth <132336480+terawuthlogicspark@users.noreply.github.com>
2026-03-11 09:08:49 +0700

    chore: update project structure and dependencies for React PDF Kit integration

--- src/app/components/AppPdfViewer.tsx
+++ src/app/components/AppPdfViewer.tsx
@@ -1,10 +1,11 @@
+"use client";
 import {
   RPProvider,
-  RPDefaultLayout,
+  RPLayout,
   RPPages,
   RPProviderProps,
   RPLayoutProps,
-} from "@pdf-viewer/react";
+} from "@react-pdf-kit/viewer";
 
 interface Props {
   showToolbar?: boolean;
@@ -17,13 +18,13 @@ const AppPdfViewer = (props: Props) => {
 
   return (
     <RPProvider
-      src="https://cdn.codewithmosh.com/image/upload/v1721763853/guides/web-roadmap.pdf"
+      src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
       {...providerProps}
     >
       {showToolbar ? (
-        <RPDefaultLayout {...defaultLayoutProps}>
+        <RPLayout toolbar {...defaultLayoutProps}>
           <RPPages />
-        </RPDefaultLayout>
+        </RPLayout>
       ) : (
         <div style={{ width: "100%", height: "550px" }}>
           <RPPages />
